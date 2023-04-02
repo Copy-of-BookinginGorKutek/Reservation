b9b7a16eeb4f7b42083812c4784c5e540ab788ba
Sasha Nabila Fortuna <sasha.nabila@ui.ac.id>
2023-04-02 19:34:34 +0700

    menambahkan anotasi repository

--- src/main/java/com/b2/reservation/repository/LapanganReservasiRepository.java
+++ src/main/java/com/b2/reservation/repository/LapanganReservasiRepository.java
@@ -3,9 +3,11 @@ package com.b2.reservation.repository;
 import com.b2.reservation.model.lapangan.LapanganReservasi;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+@Repository
 public interface LapanganReservasiRepository extends JpaRepository<LapanganReservasi, Integer>{
     @NonNull
     List<LapanganReservasi> findAll();
