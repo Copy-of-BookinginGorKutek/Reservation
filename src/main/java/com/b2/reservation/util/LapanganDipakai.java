5883aea6dac1eb87952b89c223a1d991d1f70de0
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2023-04-13 20:46:26 +0700

    add lapangandipakai class

---
+++ src/main/java/com/b2/reservation/util/LapanganDipakai.java
@@ -0,0 +1,25 @@
+package com.b2.reservation.util;
+
+import com.b2.reservation.model.lapangan.Lapangan;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class LapanganDipakai {
+    @Setter
+    @Getter
+    private Lapangan lapangan;
+    @Setter
+    @Getter
+    private LocalDateTime waktuMulai;
+    @Setter
+    @Getter
+    private LocalDateTime waktuSelesai;
+
+    public double getHours(){
+        Duration duration = Duration.between(waktuMulai, waktuSelesai);
+        return (duration.getSeconds()+1)/3600.0;
+    }
+}
